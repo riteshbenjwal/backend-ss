1484e83a24080280fff8fc0f4ce9dbd293c98382
riteshbenjwal <riteshbenjwal7@gmail.com>
2022-10-22 18:50:27 +0530

    ToString and ToNumber

---
+++ intro_to_coercion/valueof_toString_demo.js
@@ -0,0 +1,6 @@
+let a = { english: 90, maths: 767 };
+console.log(a.toString()); // returns [object ,object]
+console.log(a.valueOf()); // returns object itself
+let x = [1, 2, 3];
+console.log(x.toString());
+console.log(x.valueOf());
