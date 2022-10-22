a0648db1a9d23452f75d03a4080d3054a1f1016c
riteshbenjwal <riteshbenjwal7@gmail.com>
2022-10-22 20:19:11 +0530

    Strict equal vs equal

---
+++ coercion continued/strictEqualchecl.js
@@ -0,0 +1,15 @@
+console.log(NaN === 23);
+console.log(NaN === NaN);
+console.log(33 === 33);
+console.log(33 === 23);
+console.log(0 === -0);
+console.log(-0 === 0);
+console.log(undefined === undefined);
+console.log(null === null);
+console.log("Ritesh" === "Ritesh");
+console.log({} === {});
+let x = {};
+console.log(x === x);
+console.log(Symbol("Ritesh") === Symbol("Ritesh"));
+let y = Symbol("Ritesh");
+console.log(y === y);
