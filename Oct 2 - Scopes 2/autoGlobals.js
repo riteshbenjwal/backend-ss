e060749f35eb4ec57a712d1d6d27dca66effacc8
riteshbenjwal <riteshbenjwal7@gmail.com>
2022-10-24 16:40:05 +0530

    scopes day 2

---
+++ Oct 2 - Scopes 2/autoGlobals.js
@@ -0,0 +1,16 @@
+var teacher = "Sanket";
+function fun() {
+  var teacher = "Pulkit";
+  console.log(teacher, teachingAssistant);
+  teachingAssistant = "vibhav"; // eligible for becoming autoglobal
+}
+
+function gun() {
+  var student; // scope of gun
+  console.log(subject, typeof subject);
+  var subject = "JS";
+  console.log(teacher, student);
+}
+
+console.log(teacher);
+gun();
