b54452bf3054df323323204cd8307902536b9545
riteshbenjwal <riteshbenjwal7@gmail.com>
2023-01-14 14:00:46 +0530

    rest-api-setup

---
+++ basic_rest_api/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let blogPost = [];
+
+app.get("/blogs", (req, res) => {
+  return res.status(404).json({
+    data: blogPost,
+    success: true,
+  });
+});
+
+app.post("/blogs", (req, res) => {
+  blogPost.push({
+    title: req.body.title,
+    content: req.body.content,
+    id: Math.floor(Math.random() * 1000),
+  });
+
+  return res.status(201).json({
+    success: true,
+  });
+});
+
+app.get("/blogs/:id", (req, res) => {
+  const result = blogPost.filter((blog) => blog.id === req.params.id);
+  return res.status(200).json({
+    data: result,
+    success: true,
+  });
+});
+
+app.delete("/blogs/:id", (req, res) => {});
+
+app.listen(PORT, () => {
+  console.log("Server started on PORT", PORT);
+});
