4efac7c669384907b02748a98d09825193561a3c
riteshbenjwal <riteshbenjwal7@gmail.com>
2023-01-14 19:10:07 +0530

    intro to middleware

--- basic_rest_api/index.js
+++ basic_rest_api/index.js
@@ -6,8 +6,26 @@ const PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(logger);
 
 let blogPost = [];
+
+function logger(req, res, next) {
+  console.log(req.url);
+  console.log(req.body);
+  let condition = false;
+  if (condition) {
+    return res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+  next();
+}
+
+function isAuthenticated(req, res, next) {
+  console.log("Yes user is authenticated");
+  next();
+}
 
 app.get("/blogs", (req, res) => {
   return res.status(404).json({
@@ -16,7 +34,7 @@ app.get("/blogs", (req, res) => {
   });
 });
 
-app.post("/blogs", (req, res) => {
+app.post("/blogs", isAuthenticated, (req, res) => {
   blogPost.push({
     title: req.body.title,
     content: req.body.content,
